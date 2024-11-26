2bde4db8155f63b41f28b30d55897f823d23fb38
Losan Yuriy <losanyuriy@gmail.com>
2024-11-26 12:11:04 +0200

    third commit

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,7 @@
 "use strict";
 
 function makeTransaction(quantity, pricePerDroid) {
-  totalPrice = quantity * pricePerDroid;
+  let totalPrice = quantity * pricePerDroid;
   return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
 
