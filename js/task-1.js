c93ba3d006c64858a1f9c7f65d443c67784fd87c
Losan Yuriy <losanyuriy@gmail.com>
2025-01-25 17:09:02 +0200

    first commit

--- js/task-1.js
+++
@@ -1,9 +0,0 @@
-const categoriesList = document.querySelectorAll('.item');
-console.log(`Number of categories: ${categoriesList.length}`);
-
-categoriesList.forEach(categories => {
-  const categoriesName = categories.querySelector('h2').textContent;
-  const categoriesLength = categories.querySelectorAll('li').length;
-  console.log(`Category: ${categoriesName}`);
-  console.log(`Elements: ${categoriesLength}`);
-});
--- js/task-3.js
+++
@@ -1,7 +0,0 @@
-const nameInput = document.querySelector('#name-input');
-const nameOutput = document.querySelector('#name-output');
-
-nameInput.addEventListener('input', text => {
-  const trimmedInput = nameInput.value.trim();
-  nameOutput.textContent = trimmedInput === '' ? 'Anonymous' : trimmedInput;
-});
